985f8aa9b0f0789a229f6b68b00c49ef55d3eb15
geosmi <geosmi24@gmail.com>
2026-08-23 11:29:17 +0300

    fix: skip credentials and requests for dynamo

--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -1,10 +1,15 @@
 resource "aws_dynamodb_table" "test_table" {
-  name              = "test-table"
-  billing_mode      = "PAY_PER_REQUEST"
-  stream_enabled    = true
-  stream_view_type  = "NEW_AND_OLD_IMAGES"
-  hash_key          = "Id"
-  range_key         = "Name"
+  name                        = "test-table"
+  billing_mode                = "PAY_PER_REQUEST"
+  stream_enabled              = true
+  stream_view_type            = "NEW_AND_OLD_IMAGES"
+  hash_key                    = "Id"
+  range_key                   = "Name"
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+  skip_region_validation      = true
+  force_path_style            = true
 
   attribute {
     name = "Id"
