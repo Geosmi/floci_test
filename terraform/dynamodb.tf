a4acd9c2d1d2d82f8dc41f95723d721cb49ebeb2
geosmi <geosmi24@gmail.com>
2026-08-23 11:34:23 +0300

    fix: add dynamodb endpoints

--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -5,11 +5,6 @@ resource "aws_dynamodb_table" "test_table" {
   stream_view_type            = "NEW_AND_OLD_IMAGES"
   hash_key                    = "Id"
   range_key                   = "Name"
-  skip_credentials_validation = true
-  skip_metadata_api_check     = true
-  skip_requesting_account_id  = true
-  skip_region_validation      = true
-  force_path_style            = true
 
   attribute {
     name = "Id"
--- terraform/main.tf
+++ terraform/main.tf
@@ -29,10 +29,11 @@ provider "aws" {
   region = "us-east-1"
 
   endpoints {
-    s3     = "http://localhost:4566"
-    sqs    = "http://localhost:4566"
-    lambda = "http://localhost:4566"
-    iam    = "http://localhost:4566"
+    s3       = "http://localhost:4566"
+    dynamodb = "http://localhost:4566"
+    sqs      = "http://localhost:4566"
+    lambda   = "http://localhost:4566"
+    iam      = "http://localhost:4566"
   }
 
   access_key = "floci"
